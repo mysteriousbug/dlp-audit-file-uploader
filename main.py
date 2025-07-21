18f17538152d79bb33e7d06d553f1d07d59fc1c5
Ananya Aithal <138992924+mysteriousbug@users.noreply.github.com>
2025-07-21 14:39:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ uploaded_files = st.file_uploader("Choose files", accept_multiple_files=True)
 if uploaded_files:
     st.success(f"{len(uploaded_files)} file(s) uploaded")
 
-for file in uploaded_files:
-    st.write(f"{file.name}")
+    for file in uploaded_files:
+        st.write(f" 🗎 {file.name}")
+
+    try:
+        content = file.read().decode("utf-8")
+        st.text_area(f"Content of {file.name}:", content, height = 150)
+
+    except Exception:
+            st.warning(f"Cannot display contents of {file.name} ")
+
+else:
+     st.info("No files uploaded yet.")
