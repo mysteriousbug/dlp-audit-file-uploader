03da21134bf045f91fcf94d0aea4aa0672a9f093
Ananya Aithal <138992924+mysteriousbug@users.noreply.github.com>
2025-07-21 16:30:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from git import Repo
 
 # --- GitHub Configuration ---
 GIT_REPO = "https://github.com/mysteriousbug/dlp-audit.git"
-GIT_LOCAL_PATH = "dlp-audit"
+GIT_LOCAL_PATH = "C:/Users/2019778/Downloads/personal/dlp-audit"
 GIT_BRANCH = "main"
 GITHUB_TOKEN = st.secrets["token"]
 
